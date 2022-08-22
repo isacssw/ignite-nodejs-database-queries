7ff4a0e4e2977fc81174553c42bbc9a036fd0213
Isac Silva <Isac.Silva@criticaltechworks.com>
2022-08-22 12:08:29 +0100

    first commit

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,36 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    const games = await this.repository
+      .createQueryBuilder("game")
+      .where("game.title ilike :title", { title: `%${param}%` })
+      .getMany()
+
+    return games;
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    return this.repository.query(
+      `SELECT COUNT(id) FROM games`
+    );
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    return this.repository.createQueryBuilder("user_game")
+      .relation(Game, "users")
+      .of(id)
+      .loadMany()
+  }
+}
